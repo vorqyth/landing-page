848d1aa4ff46fd2af4f92c728e1c40924687eea3
vorqyth <vorqyth@gmail.com>
2026-05-01 20:38:50 +0300

    Update next.config.mjs configuration
    Removed TypeScript build error ignoring and added basePath.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: '/landing-page',
 }
 
 export default nextConfig
